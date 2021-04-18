d327e96071b99921e2262d443e193a8cfe1ed9ca
Severine-PromoPan <severine.ngalasso@gmail.com>
2021-04-18 16:53:50 +0200

    Admin Order Page orders table done

--- src/pages/AdminOrderPage/index.js
+++ src/pages/AdminOrderPage/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './styles.scss';
 import AdminMenu from 'src/components/AdminMenu';
 import Options from './Options';
+import OrdersTable from './OrdersTable';
 
 const AdminOrderPage = () => (
   <div className='container'>
     <AdminMenu />
     <h1>Commandes</h1>
     <Options />
-    <div>Tableau</div>
+    <OrdersTable />
   </div>
 );
 
