d7f42b76cdce54e67066b0f9aee15b77046d0f8f
Severine-PromoPan <severine.ngalasso@gmail.com>
2021-04-28 17:02:31 +0200

    cart page button to change subscription and updated store on reload

--- src/pages/CartPage/index.js
+++ src/pages/CartPage/index.js
@@ -2,27 +2,31 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
-import { Button, Form } from 'semantic-ui-react'
+import { Form } from 'semantic-ui-react'
 import DeliveryFormConfirm from 'src/containers/DeliveryFormConfirm';
 import NavFormule from 'src/containers/NavFormuleCart';
 
 // == Composant
-const CartPage = () => (
-  <div className="cartpage">
-    <h1 className="cartpage__header" >Récapitulatif de Votre Commande </h1>
-    <NavFormule />
-    <DeliveryFormConfirm />
-    <Form>
-      <Form.Checkbox className="checkboxCGV" label="J'ai lu et j'accepte les conditions générales de vente" required/>
-      <div className="cartpage__button"> 
-        <Link to="/abonnement/commande-validee">         
-          <Button icon labelPosition='right' type="submit">
-            Paiement
-          </Button> 
-        </Link>
-      </div>
-    </Form>
-  </div>
-);
+const CartPage = () => {
+
+  return (
+    <div className="cartpage">
+      <h1 className="cartpage__header" >Récapitulatif de Votre Commande </h1>
+      <a className='button-link' href='/abonnement'><button>Changer de formule</button></a>
+      <NavFormule />
+      <DeliveryFormConfirm />
+      <Form className="padding-top">
+        <Form.Checkbox className="checkboxCGV" label="J'ai lu et j'accepte les conditions générales de vente" required/>
+        <div className="cartpage__button"> 
+          <Link to="/abonnement/commande-validee">         
+            <button type="submit">
+              Paiement
+            </button> 
+          </Link>
+        </div>
+      </Form>
+    </div>
+  );
+}
 
 export default CartPage
