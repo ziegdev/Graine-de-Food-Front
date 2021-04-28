7f19f260cf4713fdd2fd0dc4462dab0323d39175
Severine-PromoPan <severine.ngalasso@gmail.com>
2021-04-28 16:22:41 +0200

    subscribe page - main pic update to match homepage

--- src/pages/SubscribePage/MainPic.js
+++ src/pages/SubscribePage/MainPic.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import pic from 'src/assets/images/terroir.jpg';
 
-
-const MainPic = () => {
-    return (
-       <img className="subscribe__pic__img" src={pic} alt="les 3 formules du terroir"></img>
-    )
-  };
+const MainPic = () => (
+  <div className="subscribe__imageContainer">
+    <h1 className="subscribe__title">Choisissez votre formule</h1>
+  </div>
+);
 
 export default MainPic;
 
