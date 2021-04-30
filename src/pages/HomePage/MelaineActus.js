0ae4e5d7d4e82742b5a696db776d32ab2dcb4511
Severine-PromoPan <severine.ngalasso@gmail.com>
2021-04-30 16:16:40 +0200

    Responsive Homepage - Selling points OK

--- src/pages/HomePage/MelaineActus.js
+++ src/pages/HomePage/MelaineActus.js
@@ -4,40 +4,39 @@ import blog1 from 'src/assets/images/IllustrationsHomepage/pexels-victoria-emers
 import blog2 from 'src/assets/images/IllustrationsHomepage/pexels-grawal-7233716.jpg';
 import blog3 from 'src/assets/images/IllustrationsHomepage/pexels-tima-miroshnichenko-6508773.jpg';
 import './styles.scss';
-
 const MelaineActus = () => (
   <>
     <h2 className="centered">Les actus de Graine de Magie</h2>
     <div className="actus">
       <Card>
-        <Image src={blog1} wrapped ui={false} as='a' href='https://grainedemagie.com/' target='_blank' />
+        <Image src='https://grainedemagie.com/wp-content/uploads/2021/04/2.jpg' wrapped ui={false} as='a' href='https://grainedemagie.com/2021/04/26/champagne-pierre-mignon-le-prestige-dans-vos-flutes/' target='_blank' />
         <Card.Content>
-          <Card.Header as='a' href='https://grainedemagie.com/' target='_blank'>Titre Article Blog 1</Card.Header>
+          <Card.Header as='a' href='https://grainedemagie.com/' target='_blank'>CHAMPAGNE PIERRE MIGNON </Card.Header>
           <Card.Description>
-            Vice paleo salvia austin, mustache keffiyeh gentrify. Tbh kombucha crucifix drinking vinegar four dollar toast, vinyl man bun church-key trust fund small batch meditation distillery scenester celiac biodiesel.
+            Bonjour à tous, J'espère qu'en cette période printanière vous vous portez bien. 🍃 Aujourd'hui, je vous présente les Champagnes Pierre Mignon. Vous pouvez vous rendre sur leur site pour découvrir leur gamme. La gamme Héritage offre fruité et rondeur grâce au Pinot Meunier. La gamme Gastronomique, quant à elle, séduira les palais les plus délicats. Enfin, la […]
           </Card.Description>
         </Card.Content>
       </Card>
       <Card>
-        <Image src={blog2} wrapped ui={false} as='a' href='https://grainedemagie.com/' target='_blank' />
+        <Image src='https://grainedemagie.com/wp-content/uploads/2020/11/IMG_9927.jpg' wrapped ui={false} as='a' href='https://grainedemagie.com/' target='_blank' />
         <Card.Content>
-          <Card.Header as='a' href='https://grainedemagie.com/' target='_blank'>Titre Article Blog 2</Card.Header>
+          <Card.Header as='a' href='https://grainedemagie.com/' target='_blank'> 
+Recette : Moelleux au chocolat côte d'or</Card.Header>
           <Card.Description>
-            Vice paleo salvia austin, mustache keffiyeh gentrify. Tbh kombucha crucifix drinking vinegar four dollar toast, vinyl man bun church-key trust fund small batch meditation distillery scenester celiac biodiesel.
+              Recette : Moelleux au chocolat côte d'or avec son coeur collant    Bonjour tout le monde ! J'espère que vous allez bien. Je reviens aujourd'hui avec une nouvelle recette, 100% gourmande : un moelleux au chocolat côte d'or… Le principe est le même que celui au Nutella que vous pouvez retrouver ici mais avec du […]
           </Card.Description>
         </Card.Content>
       </Card>
       <Card>
-        <Image src={blog3} wrapped ui={false} as='a' href='https://grainedemagie.com/' target='_blank' />
+        <Image src="https://grainedemagie.com/wp-content/uploads/2021/01/IMG_2788-2.jpg" wrapped ui={false} as='a' href='https://grainedemagie.com/' target='_blank' />
         <Card.Content>
-          <Card.Header as='a' href='https://grainedemagie.com/' target='_blank'>Titre Article Blog 3</Card.Header>
+          <Card.Header as='a' href='https://grainedemagie.com/' target='_blank'>Une détox après les fêtes avec Esprit Bio</Card.Header>
           <Card.Description>
-            Vice paleo salvia austin, mustache keffiyeh gentrify. Tbh kombucha crucifix drinking vinegar four dollar toast, vinyl man bun church-key trust fund small batch meditation distillery scenester celiac biodiesel.
+            Bonjour tout le monde ! J'espère que vous allez bien et que vous avez passé de bonnes fêtes ! Je vous souhaite une bonne année 2021. Aujourd'hui, je reviens vers vous un nouvel article détox… Je parle souvent de food sur mon blog et je me suis dit que de vous parler détox + food […]
           </Card.Description>
         </Card.Content>
       </Card>
     </div>
   </>
 );
-
 export default MelaineActus;
--- src/pages/HomePage/SellingPoints.js
+++ src/pages/HomePage/SellingPoints.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import {   Link } from 'react-router-dom';
-import { Icon, Button } from 'semantic-ui-react';
+import { Link } from 'react-router-dom';
+import { Icon } from 'semantic-ui-react';
 import './styles.scss';
 
 const SellingPoints = () => (
   <>
-    <h2>Comment ça marche, Graine de Food?</h2>
+    <h2 className="text-align-center">Comment ça marche, Graine de Food?</h2>
     <div className="homepage__selling-points">
       <div className="centered homepage__selling-points--item">
         <Icon name='computer' size='huge' />
-        <h3>Je m'abonne</h3>
-        <p>Je choisis ma formule<br/>1 mois, 3 mois ou 6 mois</p>
+        <h3 className="text-align-center h3">Je m'abonne</h3>
+        <p className="text-align-center">Je choisis ma formule<br/>1 mois, 3 mois ou 6 mois</p>
       </div>
       <div className="centered homepage__selling-points--item">
         <Icon name='box' size='huge' />
-        <h3>Je reçois ma box</h3>
-        <p>Directement à la maison<br/>en début de mois</p>
+        <h3 className="text-align-center h3">Je reçois ma box</h3>
+        <p className="text-align-center">Directement à la maison<br/>en début de mois</p>
       </div>
       <div className="centered homepage__selling-points--item">
         <Icon name='food' size='huge' />
-        <h3>Je déguste</h3>
-        <p>Je découvre des produits français de qualité</p>
+        <h3 className="text-align-center h3">Je déguste</h3>
+        <p className="text-align-center">Je découvre des produits <br/>français de qualité</p>
       </div>
     </div>
-    <a href='/abonnement'><button ><div className = "aboButton">Je m'abonne à la box Graine de Food</div></button></a>
+    <Link to='/abonnement'><button>Je m'abonne à la box Graine de Food</button></Link>
 
   </>
 );
