0f5c0b739ec32c51ea86c0ca07e860710984fb2c
Severine-PromoPan <severine.ngalasso@gmail.com>
2021-05-02 18:25:53 +0200

    responsive menu completed

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,20 +1,38 @@
+import { createMedia } from '@artsy/fresnel'
 import React from 'react';
 import { BrowserRouter as Router, Link } from 'react-router-dom';
 import logo from 'src/assets/images/Logo_grainedefood.png';
 import HeaderMenu from '/src/containers/HeaderMenu/index.js'
+import SidebarMenu from '/src/containers/SidebarMenu/index.js'
 
 import './styles.scss';
 
 const Header = () => {
+
+  const { MediaContextProvider, Media } = createMedia({
+    breakpoints: {
+      mobile: 0,
+      desktop_tablet: 760,
+    },
+  })
+
     return (
    
         <div className="header">
-            <div className="header__logo">
-                <Link to='/'>
-               <img className="header__logo__img" src={logo} alt="logo de Grainedefood" ></img>
-                </Link>
-            </div>
-            <HeaderMenu />
+          <div className="header__logo">
+              <Link to='/'>
+              <img className="header__logo__img" src={logo} alt="logo de Grainedefood" ></img>
+              </Link>
+          </div>
+
+          <MediaContextProvider>
+              <Media at="desktop_tablet">
+                <HeaderMenu />
+              </Media>
+              <Media at="mobile">
+                <SidebarMenu />
+              </Media>
+          </MediaContextProvider>
         </div>
 
     );
